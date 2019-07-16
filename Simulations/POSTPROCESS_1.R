2eb8ba22d05f809ec314313c096ce4640eeb11b1
Loris Michel <loris.michel@gmail.com>
2019-07-16 22:01:26 +0200

    uèdated post

--- Simulations/POSTPROCESS_1.R
+++ Simulations/POSTPROCESS_1.R
@@ -3,9 +3,9 @@ PATH.PLOTS <- "../Plots/"
 load(paste0(PATH.DATA, "powerStudy.Rdata"))
 
 require(data.table)
-power.table <- power.data[,.(power = mean(reject)),by=c("logn","loglambda")]
 
 png(filename = paste0(PATH.PLOTS,"sim1.png"))
-plot(power.table$logn,power.table$loglambda,cex = power.table$power*2, pch=19,xlab="log(n)",ylab="-gamma*log(n)")
+plot(power.table$logn,power.table$loglambda,cex = power.table$power_search*2, pch=19,xlab="log(n)",ylab="-gamma*log(n)")
+points(power.table$logn,power.table$loglambda,cex = power.table$power_binomial*2, pch=19,col="red")
 abline(a = 0, b = -1,col="blue")
 dev.off()
