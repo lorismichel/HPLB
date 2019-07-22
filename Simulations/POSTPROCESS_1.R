34d9c49b738caf5e3581d05b569c8590c42fe97e
Loris Michel <loris.michel@gmail.com>
2019-07-22 17:07:19 +0200

    new plot sim 1

--- Simulations/POSTPROCESS_1.R
+++ Simulations/POSTPROCESS_1.R
@@ -11,18 +11,18 @@ require(data.table)
 
 
 colfunc <- colorRampPalette(c("black", "white"))
-cols = rev(colfunc(length(seq(0.1,0.9,0.1)))
-
-
+cols = rev(colfunc(length(seq(-0.1,1.1,0.1))))
+print(cut(power.table$power_search, labels = FALSE, breaks = seq(-0.1,1.1,0.1)))
+		 
 # loglogplot
 png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_1.png"))
 par(mfrow=c(1,2))
 plot(power.table$logn,power.table$loglambda,
-     col = cols[cut(power.table$power_search, labels = FALSE, breaks = seq(0.1,0.9,0.1))],
+     col = cols[cut(power.table$power_search, labels = FALSE, breaks = seq(-0.1,1.1,0.1))],
      pch=19,xlab="log(n)",
      ylab="-gamma*log(n)")
 plot(power.table$logn,power.table$loglambda,
-     col = cols[cut(power.table$power_binomial, labels = FALSE, breaks = seq(0.1,0.9,0.1))],
+     col = cols[cut(power.table$power_binomial, labels = FALSE, breaks = seq(-0.1,1.1,0.1))],
      pch=19,xlab="log(n)",
      ylab="-gamma*log(n)")
 dev.off()
