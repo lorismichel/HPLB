e141248ecea3a60172317f8fe609eeeab37acbba
Loris Michel <loris.michel@gmail.com>
2019-07-23 11:35:46 +0200

    nrw plot 5

---
+++ Simulations/POSTPROCESS_5.R
@@ -0,0 +1,17 @@
+# POSTPROCESS 5
+# type: producing plots for the loglog scatter plot of sample size against tv
+
+# PATHS
+PATH.DATA <- "../Data/"
+PATH.PLOTS <- "../Plots/"
+load(paste0(PATH.DATA, "DATA_SIMULATION_5.Rdata"))
+
+# libs
+require(data.table)
+
+
+# loglogplot
+png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_5.png"))
+plot(power.table$p, power.table$tvhat_search, pch=19, xlab="p", ylab="tv")
+points(power.table$p,pmax(0, power.table$tvhat_binomial), pch=19,col="red")
+dev.off()
