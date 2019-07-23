288856b598a23904ff2dd8c8b7ec29a248ce4e6a
Loris Michel <loris.michel@gmail.com>
2019-07-23 13:56:49 +0200

    new sims

--- Simulations/POSTPROCESS_5.R
+++ Simulations/POSTPROCESS_5.R
@@ -14,9 +14,12 @@ require(data.table)
 png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_5.png"))
 par(mfrow=c(2,1))
 # Boston
-plot(power.table.boston$p, power.table.boston$tvhat_search, pch=19, xlab="p", ylab="tv",main="Boston",type="b")
+plot(power.table.boston$p, power.table.boston$tvhat_search, pch=19, xlab="p", ylab="tv",main="Boston dataset",type="b")
 points(power.table.boston$p,pmax(0, power.table.boston$tvhat_binomial), pch=19,col="red",type="b")
 # titanic
-plot(power.table.titanic$p, power.table.titanic$tvhat_search, pch=19, xlab="p", ylab="tv",main="titanic", type="b")
+plot(power.table.titanic$p, power.table.titanic$tvhat_search, pch=19, xlab="p", ylab="tv",main="Titanic dataset", type="b")
 points(power.table.titanic$p,pmax(0, power.table.titanic$tvhat_binomial), pch=19,col="red", type="b")
+
+plot(power.table.breast$p, power.table.breast$tvhat_search, pch=19, xlab="p", ylab="tv",main="Breast cancer dataset", type="b")
+points(power.table.breast$p,pmax(0, power.table.breast$tvhat_binomial), pch=19,col="red", type="b")
 dev.off()
