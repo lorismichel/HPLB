f2b59bfa578e1b78f232985fe748a478748ed63e
JeffNaef <jeffrey.naef@stat.math.ethz.ch>
2019-07-24 16:45:43 +0200

    Revert "Update helpers.R"
    This reverts commit 563055650291255f41b59eaee443a8648459b7b0.

--- Simulations/helpers.R
+++ Simulations/helpers.R
@@ -24,6 +24,6 @@ generateWitnessFunctionGaussianKernel <- function(sample1, sample2, rho=NULL) {
 # small example
 sample1 <- matrix(rnorm(300),ncol=1)
 sample2 <- matrix(rnorm(300,sd=2),ncol=1)
-f <- generateWitnessFunctionGaussianKernel(sample1, sample2, rho = NULL)
+f <- generateWitnessFunctionGaussianKernel(sample1, sample2, rho = 2)
 plot(seq(-4,4,0.1), sapply(seq(-4,4,0.1), function(x) f(x)),type="b",pch=19)
 
