2cf29157a0c9c3f6a4325119f8085a0f7b0413e1
JeffNaef <jeffrey.naef@stat.math.ethz.ch>
2019-07-25 08:22:30 +0200

    Update helpers.R

--- Simulations/helpers.R
+++ Simulations/helpers.R
@@ -27,3 +27,22 @@ generateWitnessFunctionGaussianKernel <- function(sample1, sample2, rho=NULL) {
 #f <- generateWitnessFunctionGaussianKernel(sample1, sample2, rho = 2)
 #plot(seq(-4,4,0.1), sapply(seq(-4,4,0.1), function(x) f(x)),type="b",pch=19)
 
+
+rtcopula <- function(n,R,df){
+
+  p <- ncol(R)
+  T <- matrix(NA, n, p)
+
+  for(i in 1:n){
+    r <- rmvt(1,R,df)
+    for(j in 1:p){
+      term1 <- pt(r[j], df)
+      term2 <- qnorm(term1)
+
+      T[i,j] <- term2
+    }
+  }
+
+  return(T)
+
+}
