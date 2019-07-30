ef073a1f1dab9eb12be9667a5f913c57ca6e0fae
Loris Michel <loris.michel@gmail.com>
2019-07-30 08:10:45 +0200

    plot sim 3

--- Simulations/POSTPROCESS_3.R
+++ Simulations/POSTPROCESS_3.R
@@ -13,15 +13,15 @@ require(data.table)
 # loglogplot
 png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_2.png"))
 par(mfrow=c(2,2))
-plot(power.table[m==0.5,]$tvhat_search,pch=19, xlab="p", ylab="tv")
+plot(power.table[m==0.5,]$tvhat_search,pch=19, xlab="p", ylab="TV", main="Null hypothesis")
 points(pmax(0, power.table[m==0.5,]$tvhat_binomial),pch=19,col="red")
 
-plot(power.table[m==2,]$tvhat_search,pch=19, xlab="p", ylab="tv")
+plot(power.table[m==2,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
 points(pmax(0, power.table[m==2,]$tvhat_binomial),pch=19,col="red")
 
-plot(power.table[m==4,]$tvhat_search,pch=19, xlab="p", ylab="tv")
+plot(power.table[m==4,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
 points(pmax(0, power.table[m==4,]$tvhat_binomial),pch=19,col="red")
 
-plot(power.table[m==10,]$tvhat_search,pch=19, xlab="p", ylab="tv")
+plot(power.table[m==10,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
 points(pmax(0, power.table[m==10,]$tvhat_binomial),pch=19,col="red")
 dev.off()
