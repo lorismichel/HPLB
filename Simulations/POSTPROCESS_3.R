01527b2fb040a47317e823ba52e19e4a0cd7d932
Loris Michel <loris.michel@gmail.com>
2019-07-30 08:25:03 +0200

    new plot

--- Simulations/POSTPROCESS_3.R
+++ Simulations/POSTPROCESS_3.R
@@ -16,12 +16,12 @@ par(mfrow=c(2,2))
 plot(power.table[m==0.5,]$tvhat_search,pch=19, xlab="p", ylab="TV", main="Null hypothesis")
 points(pmax(0, power.table[m==0.5,]$tvhat_binomial),pch=19,col="red")
 
-plot(power.table[m==2,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
-points(pmax(0, power.table[m==2,]$tvhat_binomial),pch=19,col="red")
+plot(power.table[m==1,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
+points(pmax(0, power.table[m==1,]$tvhat_binomial),pch=19,col="red")
 
-plot(power.table[m==4,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
-points(pmax(0, power.table[m==4,]$tvhat_binomial),pch=19,col="red")
+plot(power.table[m==1.5,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
+points(pmax(0, power.table[m==1.5,]$tvhat_binomial),pch=19,col="red")
 
-plot(power.table[m==10,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
-points(pmax(0, power.table[m==10,]$tvhat_binomial),pch=19,col="red")
+plot(power.table[m==5,]$tvhat_search,pch=19, xlab="p", ylab="TV",  main="Null hypothesis")
+points(pmax(0, power.table[m==5,]$tvhat_binomial),pch=19,col="red")
 dev.off()
