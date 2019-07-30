b1ed13a07a3bcd783f8bbf73f8f86174d8531bcb
Loris Michel <loris.michel@gmail.com>
2019-07-30 08:12:29 +0200

    typo

--- Simulations/POSTPROCESS_3.R
+++ Simulations/POSTPROCESS_3.R
@@ -11,7 +11,7 @@ require(data.table)
 
 
 # loglogplot
-png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_2.png"))
+png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_3.png"))
 par(mfrow=c(2,2))
 plot(power.table[m==0.5,]$tvhat_search,pch=19, xlab="p", ylab="TV", main="Null hypothesis")
 points(pmax(0, power.table[m==0.5,]$tvhat_binomial),pch=19,col="red")
