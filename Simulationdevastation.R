40e76a8dead2ff1958a1bbf932a574af892885d8
JeffNaef <jeffrey.naef@stat.math.ethz.ch>
2019-09-11 14:49:17 +0200

    simulationchanges

---
+++ Simulationdevastation.R
@@ -0,0 +1,22 @@
+#
+require(distrEx)
+M <- 10
+lambda <- 0.15
+
+TotalVarDist(Norm(mean = 0), distr::UnivarMixingDistribution(Norm(mean=0), Norm(mean=M), mixCoeff = c(1-lambda, lambda)))
+
+m <- 400
+n <- 200
+m.test <- 400
+n.test <- 100
+t <-c(rep(0,n), rep(1, m))
+x <- c(rnorm(n,mean=0),ifelse(runif(m)<=lambda, rnorm(m,mean=M), rnorm(m, mean=0)))
+t.test <-c(rep(0,n.test), rep(1, m.test))
+x.test <- c(rnorm(n.test,mean=0),ifelse(runif(m.test)<=lambda, rnorm(m.test,mean=M), rnorm(m.test, mean=0)))
+train <- data.frame(x=x,t=t)
+test <- data.frame(x=x.test,t=t.test)
+rf <- ranger::ranger(factor(t)~., train, probability = TRUE, class.weights = c(2/3,1/3))
+rho <- predict(rf, test)$predictions[,2]
+dWit::dWit(t = t.test, rho = rho, estimator.type = "binomial", s = 0.5, verbose.plot = TRUE)
+dWit::dWit(t = t.test, rho = rho, estimator.type = "asymptotic-tv-search", s = 0.5,verbose.plot = TRUE)
+dWit::dWit(t = t.test, rho = rho, estimator.type = "empirical-tv-search", s = 0.5)
--- Simulations/SIMULATION_5.R
+++ Simulations/SIMULATION_5.R
@@ -20,6 +20,10 @@ dataContamination <- function(y, p = 0) {
   y <- as.numeric(levels(y))[y]
   return(factor(ifelse(rbinom(n = length(y), size = 1, p = p) == 0, y, 1-y)))
 }
+dataContamination2 <- function(y, p = 0, preds) {
+  y <- as.numeric(levels(y))[y]
+  return(factor(ifelse(preds<=p, ifelse(rbinom(n = length(y), size = 1, p = 1/2) == 0, y, 1-y),y)))
+}
 
 
 # Prepro
@@ -45,11 +49,11 @@ res1 <- mcmapply(FUN = function(p) {
   x.train <- Boston.train
   y.test <- crim01.test
   x.test <- Boston.test
-  y.train <- dataContamination(y.train, p)
-  y.test  <- dataContamination(y.test, p)
   rf <- ranger(y~., data = data.frame(y = y.train, Boston.train),classification = TRUE, probability = TRUE)
+  y.train <- dataContamination2(y.train, p = 0.78, preds = rf$predictions[,2])
   rho <- predict(rf, data = data.frame(Boston.test))$predictions[,"1"]
-  tvhat <- dWit(t = as.numeric(levels(y.test))[y.test], rho = rho, s = 0.5, estimator.type = "tv-search")$tvhat
+  y.test  <- dataContamination2(y.test, p = 0.78, rho)
+  tvhat <- dWit(t = as.numeric(levels(y.test))[y.test], rho = rho, s = 0.5, estimator.type = "asymptotic-tv-search", verbose.plot = TRUE)$tvhat
   #tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "binomial")$tvhat
   tvhat}, grid, mc.cores = 10)
 
