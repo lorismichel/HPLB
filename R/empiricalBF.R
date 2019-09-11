77d3cf5788abf91c38c5b07ffa5d7149fee38ff4
Loris Michel <loris.michel@gmail.com>
2019-09-11 13:32:45 +0200

    added R

---
+++ R/empiricalBF.R
@@ -0,0 +1,25 @@
+#' Bounding operation
+boundingOperation <- function(v, left, right, m, n) {
+  l <- length(v)
+  if (left != 0) {
+    v <- setdiff(v,c(1:left))
+  }
+  if (right != 0) {
+    v <- setdiff(v,c((l-right+1):l))
+  }
+  return(cumsum(c(rep(1, left), c(rep(1,m),rep(0, n))[v],rep(0, right))))
+}
+
+
+
+#'obtain empirical bounding functions
+empiricalBF <- function(tv.seq, nrep = 1000, m = 100, n = 100, alpha = 0.05) {
+
+
+  l <- lapply(1:nrep, function(i) {v <- sample(1:(m+n)); lapply(tv.seq, function(tv) boundingOperation(v = v,
+                                                                     left = qbinom(prob = tv, size = m, p = 1-alpha/2),
+                                                                     right = qbinom(prob = tv, size = n, p = 1-alpha/2),
+                                                                     m = m, n = n))})
+  ll <- Reduce(x = l, f = function(x, y) mapply(x,y,FUN = function(xx,yy) pmax(xx,yy), SIMPLIFY = FALSE))
+}
+
--- R/getTvLbDistanceMatrix.R
+++ R/getTvLbDistanceMatrix.R
@@ -3,17 +3,42 @@
 #' @param ordering.array array of size (nclass, nclass, nobs) such that the value (i,j,k) represents a propensity of beeing of class j instead of i for observation k.
 #' @param alpha type I error control
 #' @param computation.type
+#' @param ...
 #' @export
 getTvLbDistanceMatrix <- function(labels,
                                   ordering.array,
                                   alpha = 0.05,
-                                  computation.type = "non-optimized") {
+                                  computation.type = "non-optimized",
+                                  ...) {
 
+  if (is.null(dim(ordering.array))) {
+    dist.mat <- matrix(0, nrow = length(unique(labels)), ncol = length(unique(labels)))
+
+    nclass <- length(unique(labels))
+
+
+    for (i in 0:(nclass-2)) {
+      for (j in (i+1):(nclass-1)) {
+
+        ind.pairs <- which(labels %in% c(i,j))
+
+        dist.mat[i+1, j+1] <- dWit(t = as.numeric(labels == j)[ind.pairs],
+                                   rho = ordering.array[ind.pairs],
+                                   s = 0.5, threshold = mean(ordering.array[ind.pairs]),...)$tvhat
+       # dist.mat[i+1, j+1] <- dWit(t = as.numeric(labels == j)[ind.pairs],
+        #                           rho = ordering.array[ind.pairs],
+        #                           s = mean(ordering.array[ind.pairs]), ...)$tvhat
+
+      }
+    }
+
+    return((dist.mat + t(dist.mat)))
+
+  }
+  # rest of tests
   if (dim(ordering.array)[1] != dim(ordering.array)[2]) {
     stop("Invalid ordering array.")
   }
-  # rest of tests
-
 
 
   # returned value
@@ -31,8 +56,7 @@ getTvLbDistanceMatrix <- function(labels,
 
         dist.mat[i+1, j+1] <- dWit(t = as.numeric(labels == j)[ind.pairs],
                                     rho = ordering.array[i+1,j+1,ind.pairs],
-                                    s = 0.5, alpha = alpha,
-                                    estimator.type = "tv-search")$tvhat
+                                    s = 0.5, ...)$tvhat
 
       }
     }
