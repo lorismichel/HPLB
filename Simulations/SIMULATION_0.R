2d921786084891a5b436e3950435447a843b6d24
Loris Michel <loris.michel@gmail.com>
2019-09-20 10:33:23 +0200

    changed dWit plus sim 0

--- Simulations/SIMULATION_0.R
+++ Simulations/SIMULATION_0.R
@@ -15,9 +15,17 @@ sampleExpContamination <- function(n, lambda) {
 }
 bayesRatio <- function(x, lambda) { dexp(x) / (lambda*dexp(-x)+(1-lambda)*dexp(x) + dexp(x))}
 
-generateInputData <- function(n, lambda) {
-  rho1 <- bayesRatio(sampleExpContamination(n = n, lambda = 0), lambda = lambda)
-  rho2 <- bayesRatio(sampleExpContamination(n = n, lambda = lambda), lambda = lambda)
+generateInputData <- function(n, lambda, imbalance.level = 0) {
+  if (imbalance.level == 0) {
+    rho1 <- bayesRatio(sampleExpContamination(n = n, lambda = 0), lambda = lambda)
+    rho2 <- bayesRatio(sampleExpContamination(n = n, lambda = lambda), lambda = lambda)
+  } else if (imbalance.level == 1) {
+   rho1 <- bayesRatio(sampleExpContamination(n = n, lambda = 0), lambda = lambda)
+   rho2 <- bayesRatio(sampleExpContamination(n = n-sqrt(n), lambda = lambda), lambda = lambda)
+  } else {
+   rho1 <- bayesRatio(sampleExpContamination(n = n, lambda = 0), lambda = lambda)
+   rho2 <- bayesRatio(sampleExpContamination(n = n-(n/2), lambda = lambda), lambda = lambda)
+  }
   t = c(rep(1, length(rho1)), rep(0, length(rho2)))
   rho <- c(rho1, rho2)
   return(list(t = t, rho = rho))
@@ -30,6 +38,8 @@ generateInputData <- function(n, lambda) {
 nrep <- 50
 #grid.gamma <- rep(seq(0.1,  1.2, by = 0.1), nrep)
 grid <- rep(10^{seq(2,5,length.out = 10)}, nrep)
+grid.imbalance.level <- c(0,1,2)
+grid <- expand.grid(grid,grid.imbalance.level)
 #grid <- expand.grid(grid.n, grid.gamma)
 
 # running simulations
@@ -38,19 +48,27 @@ res1 <- mcmapply(FUN = function(n, gamma) {
   inputs <- generateInputData(n, lambda = 0)
   tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "asymptotic-tv-search")$tvhat
   #tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "binomial")$tvhat
-  tvhat}, grid, mc.cores = 25)
+  tvhat}, grid[,1], grid[,2], mc.cores = 25)
 
 set.seed(123, "L'Ecuyer")
 res2 <- mcmapply(FUN = function(n, gamma) {
   inputs <- generateInputData(n, lambda = 0)
   #tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "tv-search")$tvhat
   tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "binomial")$tvhat
-  tvhat}, grid, mc.cores = 25)
+  tvhat}, grid[,1], grid[,2], mc.cores = 25)
+
+set.seed(123, "L'Ecuyer")
+res3 <- mcmapply(FUN = function(n, gamma) {
+  inputs <- generateInputData(n, lambda = 0)
+  #tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "tv-search")$tvhat
+  tvhat <- dWit(t = inputs$t, rho = inputs$rho, s = 0.5, estimator.type = "hyper-tv-search", z = n)$tvhat
+  tvhat}, grid[,1], grid[,2], mc.cores = 25)
 
 # gathering data
-power.data <- data.table(logn = log(grid, base = 10),
-                         tv_search = res1, tv_binomial = res2)
-power.table <- power.data[,.(power_search = mean(tv_search>0), power_binomial = mean(tv_binomial>0)),by=c("logn")]
+power.data <- data.table(logn = log(grid[,1], base = 10),
+			 imbalance = grid[,2],
+                         tv_search = res1, tv_binomial = res2, tv_hyper = res3)
+power.table <- power.data[,.(power_search = mean(tv_search>0), power_binomial = mean(tv_binomial>0)), power_hyper = mean(tv_hyper>0),by=c("logn","imbalance")]
 
 # saving results of simulations
 save(power.data, power.table, file = paste0(PATH.SAVE, "DATA_SIMULATION_0.Rdata"))
