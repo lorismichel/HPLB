48b5d61aa2b9870a8fa611a731e44e629c98245e
Loris Michel <loris.michel@gmail.com>
2019-09-25 18:05:04 +0200

    changed post 0

--- Simulations/POSTPROCESS_0.R
+++ Simulations/POSTPROCESS_0.R
@@ -24,10 +24,10 @@ plot(power.table$logn,power.table$imbalance,
 plot(power.table$logn,power.table$imbalance,
      col = cols[cut(power.table$power_binomial, labels = FALSE, breaks = seq(-0.1,1.1,0.1))],
      pch=19,xlab="log(n)",
-     ylab="imbalance", main="binomial test",font.main=2)
+     ylab="imbalance", main="binomial-test",font.main=2)
 plot(power.table$logn,power.table$imbalance,
      col = cols[cut(power.table$power_hyper, labels = FALSE, breaks = seq(-0.1,1.1,0.1))],
      pch=19,xlab="log(n)",
-     ylab="imbalance", main="hyper-tv-search",font.main=2)
+     ylab="imbalance", main="hypergeometric-test",font.main=2)
 #plot(seq(-0.1,1.1,0.1), rep(1, length(seq(-0.1,1.1,0.1))), col=cols,pch=19,cex=3)
 dev.off()
