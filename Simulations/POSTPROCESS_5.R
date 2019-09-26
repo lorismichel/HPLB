07230a8720817b3e72b18df19a3e0b2236443e5b
Loris Michel <loris.michel@gmail.com>
2019-09-26 12:55:12 +0200

    final plot 5

--- Simulations/POSTPROCESS_5.R
+++ Simulations/POSTPROCESS_5.R
@@ -16,31 +16,31 @@ png(filename = paste0(PATH.PLOTS,"PLOT_SIMULATION_5.png"))
 par(mfrow=c(3,3))
 
 # Boston
-plot(power.table.boston$p, power.table.boston$tvhat_search, ylim=c(0,1),pch=19, xlab="p", ylab="TV",main="Boston",type="b", cex = 0.7, font.main=3)
-points(power.table.boston$p,pmax(0, power.table.boston$tvhat_binomial), pch=19,col="red",type="b")
+plot(power.table.boston$p, power.table.boston$tvhat_search, ylim=c(0,1),pch=19, xlab="p", ylab="TV",main="Boston",type="l", cex = 0.7, font.main=3)
+points(power.table.boston$p,pmax(0, power.table.boston$tvhat_binomial), pch=19,col="red",type="l")
 
 # titanic
-plot(power.table.titanic$p, power.table.titanic$tvhat_search, pch=19, ylim=c(0,1), xlab="p", ylab="TV",main="Titanic", type="b", cex = 0.7, font.main=3)
-points(power.table.titanic$p,pmax(0, power.table.titanic$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.titanic$p, power.table.titanic$tvhat_search, pch=19, ylim=c(0,1), xlab="p", ylab="TV",main="Titanic", type="l", cex = 0.7, font.main=3)
+points(power.table.titanic$p,pmax(0, power.table.titanic$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.breast$p, power.table.breast$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Breast Cancer", type="b", cex = 0.7, font.main=3)
-points(power.table.breast$p,pmax(0, power.table.breast$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.breast$p, power.table.breast$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Breast Cancer", type="l", cex = 0.7, font.main=3)
+points(power.table.breast$p,pmax(0, power.table.breast$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.iono$p, power.table.iono$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Ionosphere", type="b", cex = 0.7, font.main=3)
-points(power.table.iono$p,pmax(0, power.table.iono$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.iono$p, power.table.iono$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Ionosphere", type="l", cex = 0.7, font.main=3)
+points(power.table.iono$p,pmax(0, power.table.iono$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.abalone$p, power.table.abalone$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Abalone", type="b", cex = 0.7, font.main=3)
-points(power.table.abalone$p,pmax(0, power.table.abalone$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.abalone$p, power.table.abalone$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Abalone", type="l", cex = 0.7, font.main=3)
+points(power.table.abalone$p,pmax(0, power.table.abalone$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.adult$p, power.table.adult$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Adult", type="b", cex = 0.7, font.main=3)
-points(power.table.adult$p,pmax(0, power.table.adult$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.adult$p, power.table.adult$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Adult", type="l", cex = 0.7, font.main=3)
+points(power.table.adult$p,pmax(0, power.table.adult$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.banknotes$p, power.table.banknotes$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Bank Notes", type="b", cex = 0.7, font.main=3)
-points(power.table.banknotes$p,pmax(0, power.table.banknotes$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.banknotes$p, power.table.banknotes$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Bank Notes", type="l", cex = 0.7, font.main=3)
+points(power.table.banknotes$p,pmax(0, power.table.banknotes$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.default$p, power.table.default$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Default (ISLR)", type="b", cex = 0.7, font.main=3)
-points(power.table.default$p,pmax(0, power.table.default$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.default$p, power.table.default$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="Default (ISLR)", type="l", cex = 0.7, font.main=3)
+points(power.table.default$p,pmax(0, power.table.default$tvhat_binomial), pch=19,col="red", type="l")
 
-plot(power.table.credit$p, power.table.credit$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="German Credit Default", type="b", cex = 0.7, font.main=3)
-points(power.table.credit$p,pmax(0, power.table.credit$tvhat_binomial), pch=19,col="red", type="b")
+plot(power.table.credit$p, power.table.credit$tvhat_search, ylim=c(0,1), pch=19, xlab="p", ylab="TV",main="German Credit Default", type="l", cex = 0.7, font.main=3)
+points(power.table.credit$p,pmax(0, power.table.credit$tvhat_binomial), pch=19,col="red", type="l")
 dev.off()
