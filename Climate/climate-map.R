8d1bd615eb69684d2115b9e462355f69d4b6aad7
Loris Michel <loris.michel@gmail.com>
2020-02-18 14:18:43 +0100

    added climate-map for final run

--- Climate/climate-map.R
+++ Climate/climate-map.R
@@ -1,15 +1,15 @@
-# climate map
+# Climatic map analysis
 
 # options
-PATH.CLIMATE.DATA <- "C:/Users/jeffr/Downloads/reanalysis_jeff_loris/"
+PATH.CLIMATE.DATA <- "~/Downloads/reanalysis_jeff_loris/"
 
 
-PRODUCE.PLOTS <- TRUE
-RUN.ANALYSIS <- FALSE
+PRODUCE.PLOTS       <- FALSE
+RUN.ANALYSIS        <- TRUE
 RUN.COMBINED.METHOD <- TRUE
 
 PREPRO <- 2
-SPLIT.TRAIN.TEST <- 0
+SPLIT.TRAIN.TEST <- 1
 
 
 # source
@@ -28,45 +28,57 @@ d <- climatePrePro(PATH.CLIMATE.DATA)
 
 
 # create the data
-data <- list()
-data$air <- d$air
+data       <- list()
+data$air   <- d$air
 data$prate <- d$prate
-data$shum <- d$shum
-data$mslp <- d$mslp
+data$shum  <- d$shum
+data$mslp  <- d$mslp
 
 # preprocessing
 if (PREPRO == 1) {
-  data$air   <- apply(data$air, 2, rank)
-  data$shum  <- apply(data$shum, 2, rank)
-  data$prate <- apply(data$prate, 2, rank)
-  data$mslp  <- apply(data$mslp, 2, rank)
+  data$air   <- apply(data$air, 1, rank)
+  data$shum  <- apply(data$shum, 1, rank)
+  data$prate <- apply(data$prate, 1, rank)
+  data$mslp  <- apply(data$mslp, 1, rank)
 } else if (PREPRO == 2) {
-  data$air   <- apply(data$air, 2, diff)
-  data$shum  <- apply(data$shum, 2, diff)
-  data$prate <- apply(data$prate, 2, diff)
-  data$mslp  <- apply(data$mslp, 2, diff)
+  data$air   <- apply(data$air, 1, diff)
+  data$shum  <- apply(data$shum, 1, diff)
+  data$prate <- apply(data$prate, 1, diff)
+  data$mslp  <- apply(data$mslp, 1, diff)
 } else if (PREPRO == 3) {
-  data$air   <- apply(data$air, 2, function(x) rank(diff(x)))
-  data$shum  <- apply(data$shum, 2, function(x) rank(diff(x)))
-  data$prate <- apply(data$prate, 2, function(x) rank(diff(x)))
-  data$mslp  <- apply(data$mslp, 2, function(x) rank(diff(x)))
+  data$air   <- apply(data$air, 1, function(x) rank(diff(x)))
+  data$shum  <- apply(data$shum, 1, function(x) rank(diff(x)))
+  data$prate <- apply(data$prate, 1, function(x) rank(diff(x)))
+  data$mslp  <- apply(data$mslp, 1, function(x) rank(diff(x)))
 }
 
 
-# looping
-p <- nrow(data$air)
-n <- ncol(data$air)-1
+# getting the dimensions
+p <- ncol(data$air)
+n <- nrow(data$air)
 
-resmat <- matrix(nrow=p,ncol=ifelse(RUN.COMBINED.METHOD, 3, 2))
-tvseq <- sort(unique(c(seq(0,1,by=0.01),seq(0,0.1,by=0.001),seq(0,0.2,by=0.005),c(0,0.0001,0.001,0.002,0.005,0.01,0.015,0.02))))
+
+
+# results
+resmat <- matrix(nrow=p,
+                 ncol = ifelse(RUN.COMBINED.METHOD,
+                               3,
+                               2))
+# tv seq
+tvseq <- sort(unique(c(seq(0,1,by=0.01),
+                       seq(0,0.1,by=0.001),
+                       seq(0,0.2,by=0.005),
+                       c(0,0.0001,0.001,0.002,0.005,0.01,0.015,0.02))))
 
 if (RUN.ANALYSIS) {
 
   # main for loop
   for (k in 1:p){
 
-    X <- cbind(data$air[k,], data$mslp[k,],
-               data$prate[k,], data$shum[k,])
+    X <- cbind(data$air[,k],
+               data$mslp[,k],
+               data$prate[,k],
+               data$shum[,k])
 
     # splits train-test
     if (SPLIT.TRAIN.TEST == 0) {
@@ -77,37 +89,47 @@ if (RUN.ANALYSIS) {
       ind.test <- (1:n)[-ind.train]
     }
 
+    # creating the data and fitting forest
     Y <- as.factor((1:n)>round(n/2))
-
     rf <- ranger(formula = y~., data = data.frame(x=X[ind.train,], y=Y[ind.train]), probability = TRUE)
-
     pred <- predict(rf, data = data.frame(x=X[ind.test,]))$predictions[,"TRUE"]
 
     tvhat_bin <- dWit(t=as.numeric(Y[ind.test])-1, rho = pred, tv.seq  = tvseq,
                       estimator.type = "binomial-test")$tvhat
     tvhat_search <- dWit(t=as.numeric(Y[ind.test])-1, rho = pred, tv.seq  = tvseq,
                          estimator.type = "asymptotic-tv-search")$tvhat
+
     if (RUN.COMBINED.METHOD) {
-      tvhat_combined <- dWit(t=as.numeric(Y[ind.test])-1, rho = pred, tv.seq  = c(tvhat_bin,Filter(x = tvseq, f = function(x) x > tvhat_bin)),
+      tvhat_combined <- dWit(t=as.numeric(Y[ind.test])-1, rho = pred,
+                             tv.seq  = c(tvhat_bin, Filter(x = tvseq, f = function(x) x > tvhat_bin)),
                              estimator.type = "asymptotic-tv-search")$tvhat
-      resmat[k,] <- c(tvhat_bin, tvhat_search, tvhat_combined)
+      resmat[k,] <- c(tvhat_bin,
+                      tvhat_search,
+                      tvhat_combined)
     } else {
-      resmat[k,] <- c(tvhat_bin, tvhat_search)
+      resmat[k,] <- c(tvhat_bin,
+                      tvhat_search)
     }
 
     # verbose
-    if (k%%10==0) {
+    if (k%%10 == 0) {
       print(k)
     }
   }
 
   # saving the data
-  save(resmat, file = paste0("Data/DATA_CLIMATE_MAP_SPLIT_", SPLIT.TRAIN.TEST, "_PREPRO_", PREPRO, ".Rdata"))
+  save(resmat,
+       file = paste0("Data/DATA_CLIMATE_MAP_SPLIT_",
+                     SPLIT.TRAIN.TEST,
+                     "_PREPRO_",
+                     PREPRO,
+                     ".Rdata"))
 
 }
 
 # get coordinates
-matrix.coords <- matrix(nrow=nrow(data$shum),ncol=2)
+matrix.coords <- matrix(nrow = ncol(data$shum),
+                        ncol = 2)
 
 for (i in 1:nrow(data$shum)) {
   matrix.coords[i,] <- xyFromCell(object = d$shum_raster, i)
@@ -121,7 +143,11 @@ if (PRODUCE.PLOTS) {
 
 
   # load the data
-  info <- load(paste0("Data/DATA_CLIMATE_MAP_SPLIT_", SPLIT.TRAIN.TEST, "_PREPRO_", PREPRO, ".Rdata"))
+  info <- load(paste0("Data/DATA_CLIMATE_MAP_SPLIT_",
+                      SPLIT.TRAIN.TEST,
+                      "_PREPRO_",
+                      PREPRO,
+                      ".Rdata"))
 
   # produce the plots
   library("ggmap")
@@ -132,7 +158,12 @@ if (PRODUCE.PLOTS) {
   x <-  matrix.coords[,1]-180
   y <-  matrix.coords[,2]
 
-  png(filename = paste0("Plots/PLOT_CLIMATE_MAP_SPLIT_", SPLIT.TRAIN.TEST, "_PREPRO_", PREPRO, "_PLOT_1"), width = 1000)
+  png(filename = paste0("Plots/PLOT_CLIMATE_MAP_SPLIT_",
+                        SPLIT.TRAIN.TEST,
+                        "_PREPRO_",
+                        PREPRO,
+                        "_PLOT_1"),
+      width = 1000)
   # # binomial tv
   mp_bin <- NULL
   mapWorld <- borders("world", colour="gray50", fill="gray50") # create a layer of borders
